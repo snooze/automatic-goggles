4852e0457251f7c510f73f69d61e6c3f432311e2
snooze <swales@gmail.com>
2022-05-03 08:31:38 -0400

    first commit

---
+++ pages/api/movie/[id].js
@@ -0,0 +1,16 @@
+
+import { connectToDatabase } from '../../../util/mongodb';
+import { ObjectId } from 'mongodb';
+
+export default async (req, res) => {
+  const { db } = await connectToDatabase();
+
+  const o_id = ObjectId(req.query.id);
+
+  const movies = await db
+    .collection("movies")
+    .findOne({ _id: o_id })
+
+  res.json(movies);
+};
+
