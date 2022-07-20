706469ade696c0a168dda090d2b1a30be45234f7
snooze <swales@gmail.com>
2022-07-19 22:24:22 -0400

    Try again 3

--- pages/test.js
+++
@@ -1,8 +0,0 @@
-//import { Buffer } from 'buffer';
-
-const buf = Buffer.from('hello world', 'utf8');
-
-console.log(buf.toString('hex'));
-// Prints: 68656c6c6f20776f726c64
-console.log(buf.toString('base64'));
-// Prints: aGVsbG8gd29ybGQ=
